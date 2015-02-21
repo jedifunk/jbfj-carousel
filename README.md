29f44334f250a947f8a0b6e80ffcdb1fe7f3b7af
jedifunk <iam@bryceflory.com>
2015-02-21 11:56:08 -0600

    Major updates
    From GPS

--- README.md
+++ README.md
@@ -1,55 +1,148 @@
-JBFJ Slider
-=============
+#JBFJ Slider
 
-By Bryce Flory
+By Bryce Flory, Chad Sanda and Cameron Huntington.
 
-Wordpress content slider plugin.  Fully responsive.
+Fully responsive Wordpress content slider plugin.
 
 This plugin uses the great bxslider.js jQuery plugin by Steven Wanderski.
 View the plugin on GitHub at https://github.com/wandoledzep/bxslider-4
 
-Version: 1.5.2
+*Version 1.7.3*
 
-=============
-Feature additions coming soon:
+
+##Feature Ideas
+
 - Add Video embeds
 - Add HTML content
 - Custom pager location
 - Replace control images with custom icon font options
-- Move to OOP
 
-=============
-Change Log:
+##Documentation
 
-1.5.2: February 5, 2014
+### Use
 
-	- Fixed JS to handle slider with only one slide.
+#####Shortcode
 
-1.5.1: February 5, 2014
+	[slider slug=yourSlug]
 
-	- Moved to enqueuing scripts and styles only when slider function is called.  Prevents assets from loading on pages without a slider.
-	- Changed slider JS filename to be more descriptive.
+#####Template Tag
+Add this to your page template file. Takes one arguement, slider slug name
 
-1.5: January 20, 2014
+	<?php jbfj_slider( 'yourSlugName' ); ?>
+
+###Inputs
+
+#####Slide Width
+Accepts integers and translates them to pixel values e.g. 200 -> 200px. If set to 0 (default), slides will be 100% width of their container.
+
+#####Radios
+If a radio option is not selected, that property will not save to the database, and will return as null (false).
+
+
+##Change Log
+
+*1.7.3: October 14, 2014*
+
+- Bug fix: Error on new slideshow ... calling invalid arguement that didnt exist. Removed.
+- Bug fix: Error on deleting slideshow
+
+*1.7.2: October 10, 2014*
+
+- Fix bug where new sliders create on slide page didn't add settings metadata to database
+- Moved Create and Edit forms into separate file for better readability
+- Updated Read Me to include template tag documentation
+
+*1.7.1: October 7, 2014*
+
+- Added delete functionality
+- Defaults stored in an array
+- Radios replaced check boxes to be always present in the $_POST variable
+- Slider settings are now submitted as components of the associative array [slideshow_settings]
+- Add/update now loops over $_POST variable
+
+*1.7: October 7, 2014*
+
+ - Added Ticker functionality
+ - Added new options for use with Ticker and Carousel: minSlides, maxSlides and slideWidth
+ - Refactored and reduced database calls in jbfj-slider.php
+ - Removed redundancies in jbfj-slider-script.js
+ - Refactored checkboxes to output as boolean values instead of integers
+ - Renamed container div class to jbfj-carousel to not conflict with Bootstrap naming conventions
+ - Removed redundancies for the update_metadata calls in jbfj-slider-settings.php
+ - Updated BxSlider to latest version, 4.1.2
+
+*1.6.6: September 10, 2014*
+
+ - Added apply_filters to the caption area so it will execute shortcodes
+
+*1.6.5: August 26, 2014*
+
+ - Hooked up slide url so that when it is added on the custom post type it actually puts a link on the front page
+
+*1.6.4: June 3, 2014*
+
+ - Removed unnecessary definition for plugin_dir_path
+ - Added if statement to caption div to not output if no content
+ - Bug Fix: added wp_reset_postdata() to output to properly close loop
+
+*1.6.3: March 17, 2014*
+
+ - Fixed issue with multiple sliders not working in multisite
+
+*1.6.2: February 25, 2014*
+
+ - Added shortcode functionality [slider slug=yourSlug]
+
+*1.6.1: February 25, 2014*
+
+ - Changed carousel-caption class to jbfj-carousel-caption
+
+*1.6: February 20, 2014*
+
+ - Added Multisite compatibility
+
+*1.5.2: February 5, 2014*
+
+ - Fixed JS to handle slider with only one slide.
+
+*1.5.1: February 5, 2014*
+
+ - Moved to enqueuing scripts and styles only when slider function is called.  Prevents assets from loading on pages without a slider.
+ - Changed slider JS filename to be more descriptive.
+
+*1.5: January 20, 2014*
 
  - Slider settings are no longer global. Each slider has its own settings
 
-1.3.1: December 13, 2013
+*1.3.1: December 13, 2013*
 
  - Fixed broken file paths to reflect new directory structure
 
-1.3: May 8, 2013
+*1.3: May 8, 2013*
 
  - Added support for multiple sliders using custom taxonomy
 
-1.2: May 7, 2013
+*1.2: May 7, 2013*
 
  - Added Custom URLs
  
-1.1: May 3, 2013
+*1.1: May 3, 2013*
 
  - Added Captioning
  
-1.0: May 1, 2013
+*1.0: May 1, 2013*
+
+ - Initial Release
+ Added support for multiple sliders using custom taxonomy
+
+*1.2: May 7, 2013*
+
+ - Added Custom URLs
+ 
+*1.1: May 3, 2013*
+
+ - Added Captioning
+ 
+*1.0: May 1, 2013*
 
  - Initial Release
--- jbfj-slider.php
+++ jbfj-slider.php
@@ -1,14 +1,11 @@
 <?php
 /*
-	Plugin Name: JBFJ Slider
-	Description: Responsive Slider using the BX Slider
-	Author: Bryce Flory
-	Author URI: http://bryceflory.com
-	Version: 1.5.2
+    Plugin Name: JBFJ Slider
+    Description: Custom Slider
+    Author: Bryce Flory, Chad Sanda and Cameron Huntington
+    Author URI: http://bryceflory.com
+    Version: 1.7.3
 */
-
-// Define Constants
-define( 'jbfj_PATH', plugin_dir_path(__FILE__) );
 
 // Create CPT for Slides
 add_action( 'init', 'register_cpt_slide' );
@@ -39,7 +36,7 @@ function register_cpt_slide() {
         'show_ui' => true,
         'show_in_menu' => true,
         'menu_position' => 20,
-        'menu_icon' => plugins_url('jbfj-slider/lib/img/images-stack.png'),
+        'menu_icon' => plugins_url('jbfjwp-slider/lib/img/images-stack.png'),
         'show_in_nav_menus' => false,
         'publicly_queryable' => false,
         'exclude_from_search' => true,
@@ -91,109 +88,171 @@ function register_taxonomy_slideshows() {
 }
 
 
-// Create New SQL DB Table
-function jbfj_slider_create_table() {
-	global $wpdb;
-	
-	$type = 'slideshows';
-	$table_name = $wpdb->prefix . $type . 'meta';
-		
-	  $sql = "CREATE TABLE IF NOT EXISTS {$table_name} (
-	  	meta_id bigint(20) NOT NULL AUTO_INCREMENT,
-	  	{$type}_id bigint(20) NOT NULL default 0,
+// Activate plugin with multisite check
+register_activation_hook( __FILE__, 'jbfj_slider_activate' );
 
-		meta_key varchar(255) DEFAULT NULL,
-		meta_value tinytext DEFAULT NULL,
-	   		
-	  	UNIQUE KEY meta_id (meta_id)
-	);";
+function jbfj_slider_activate($networkwide) {
+    global $wpdb;
 
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-	dbDelta($sql);
+    if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+        if ($networkwide) {
+            $old_blog = $wpdb->blogid;
+            $blogids = $wpdb->get_col("SELECT blog_id FROM {$wpdb->blogs}");
+            foreach ( $blogids as $blog_id ) {
+                switch_to_blog($blog_id);
+                jbfj_slider_table_activation();
+            }
+            switch_to_blog($old_blog);
+            return;
+        }
+    }
+    jbfj_slider_table_activation();
 }
 
-// this hook will cause our creation function to run when the plugin is activated
-register_activation_hook( __FILE__, 'jbfj_slider_create_table' );
+// Prep for table creation
+function jbfj_slider_table_activation() {
+    global $wpdb;
+
+    $type = 'slideshows';
+    $table_name = $wpdb->prefix . $type . 'meta';
+
+    if ( $wpdb->get_var( "SHOW TABLES LIKE '{$table_name}'" ) != $table_name ) {
+        jbfj_slider_create_table( $table_name, $type );
+    }
+}
+
+// Create Table & activate plugin when new site added to multisite
+add_action( 'wpmu_new_blog', 'new_blog', 10, 6);
+
+function new_blog( $blog_id, $user_id, $domain, $path, $site_id, $meta ) {
+    global $wpdb;
+
+    if ( is_plugin_active_for_network( 'jbfjwp-slider/jbfj-slider.php' ) ) {
+        $old_blog = $wpdb->blogid;
+        switch_to_blog( $blog_id );
+        jbfj_slider_table_activation();
+        switch_to_blog( $old_blog );
+    }
+}
+
+// Create custom table
+function jbfj_slider_create_table( $table_name, $type ) {
+    global $wpdb;
+
+    $sql = "CREATE TABLE IF NOT EXISTS {$table_name} (
+        meta_id bigint(20) NOT NULL AUTO_INCREMENT,
+        {$type}_id bigint(20) NOT NULL default 0,
+        meta_key varchar(255) DEFAULT NULL,
+        meta_value tinytext DEFAULT NULL,
+        UNIQUE KEY meta_id (meta_id)
+    );";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
 
 // Initialize Plugin
 add_action('init', 'jbfj_slider_init');
 
 function jbfj_slider_init() {
-	global $wpdb;
-	$variable_name = $type . 'meta';
-	$wpdb->$variable_name = $table_name;
-	$wpdb->slideshowsmeta = $wpdb->prefix . 'slideshowsmeta';
+    global $wpdb;
+    $variable_name = $type . 'meta';
+    $wpdb->$variable_name = $table_name;
+    $wpdb->slideshowsmeta = $wpdb->prefix . 'slideshowsmeta';
 }
 
 //Add Javascript for Slider
 add_action('wp_enqueue_scripts', 'jbfj_slider_scripts' );
 
 function jbfj_slider_scripts() {
-	
-	if ( !is_admin() ) {
-	
-		wp_register_style('jbfjslider_styles', plugins_url('lib/css/jbfj-slider.css', __FILE__));
-		
-		wp_register_script('bxslider_script', plugins_url('lib/js/jquery.bxslider.min.js', __FILE__), array('jquery'), false, true );
-		wp_register_script('jbfj-slider_script', plugins_url('lib/js/jbfj-slider-script.js', __FILE__), array('jquery'), false, true );
-
-	}
-	
+    if ( !is_admin() ) {
+        wp_register_style('jbfjslider_styles', plugins_url('lib/css/jbfj-slider.css', __FILE__));
+        wp_register_script('bxslider_script', plugins_url('lib/js/jquery.bxslider.min.js', __FILE__), array('jquery'), false, true );
+        wp_register_script('jbfj-slider_script', plugins_url('lib/js/jbfj-slider-script.js', __FILE__), array('jquery'), false, true );
+    }
 }
-
 
 /******************* SLIDE ADMIN & SETTINGS SECTIONS ****************/
 if ( is_admin() ) {
-	require_once( 'lib/jbfj-slider-admin.php' );
-	require_once( 'lib/jbfj-slider-settings.php' );
+    require_once( 'lib/jbfj-slider-admin.php' );
+    require_once( 'lib/jbfj-slider-settings.php' );
+    require_once( 'lib/jbfj-slider-settings-forms.php' );
 }
 
 /******************* FRONT END OUTPUT *******************/
-//Display functionality
 function jbfj_slider( $slideshow = '' ) {
-	include('lib/jbfj-slider-output.php');	
-	global $slideshows;	
-	global $wpdb;
+
+    global $slideshows;
+    global $wpdb;
+    
+    // Rename Name to slug via string replacement and lowercasing
+    $slideshow = strtolower(str_replace(' ', '-', $slideshow));
+    
+    // Include the output file
+    include('lib/jbfj-slider-output.php');
+    
+    // Get our slideshow and pull settings from the DB
+    $term = get_term_by( 'slug', $slideshow, 'slideshows' );
+    $table_name = $wpdb->prefix . 'slideshowsmeta';
+    $slideshowid = $term->term_id;
+
+    // Get meta_keys and meta_values columns, combine to $settings associative array
+    $slider_keys = $wpdb->get_col( "SELECT meta_key from {$table_name} WHERE slideshows_id = {$slideshowid}");
+    $slider_values = $wpdb->get_col( "SELECT meta_value from {$table_name} WHERE slideshows_id = {$slideshowid}");
+    $slider_settings = array_combine($slider_keys, $slider_values);
+
+    // Hack for old versions of PHP to use boolval
+	if (!function_exists('boolval')) {
+        function boolval($val) {
+                return (bool) $val;
+        }
 	
-	$term = get_term_by( 'name', $slideshow, 'slideshows' );
-	$slideshowid = $term->term_id;	
-	$table_name = $wpdb->prefix . 'slideshowsmeta';
-	
-	$slider_mode = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_mode'" );
-	$slider_speed = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_speed'" );
-	$slider_pause = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_pause'" );
-	$slider_useCSS = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_useCSS'" );
-	$slider_pager = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_pager'" );
-	$slider_controls = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_controls'" );
-	$slider_hover = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_hover'" );
-	$slider_ticker = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_ticker'" );
-	$slider_tHover = $wpdb->get_var( "SELECT meta_value FROM wp_slideshowsmeta WHERE slideshows_id = {$slideshowid} AND meta_key= 'slider_tHover'" );
-	
-	$slideshows[$slideshow] = array(
-		'jbfjslidermode' => $slider_mode,
-		'jbfjsliderspeed' => $slider_speed,
-		'jbfjsliderpause' => $slider_pause,
-		'jbfjslideruseCSS' => $slider_useCSS,
-		'jbfjsliderpager' => $slider_pager,
-		'jbfjslidercontrols' => $slider_controls,
-		'jbfjsliderhover' => $slider_hover,
-		'jbfjsliderticker' => $slider_ticker,
-		'jbfjslidertHover' => $slider_tHover
-	);
-	
-	wp_enqueue_script('bxslider_script');
-	wp_enqueue_script('jbfj-slider_script');
-	wp_enqueue_style('jbfjslider_styles');
+	}
+
+    // Store each sliders settings in a multi-dimensional array
+    $slideshows[$slideshow] = array(
+        'jbfjslidermode' => $slider_settings['slider_mode'],
+        'jbfjsliderspeed' => $slider_settings['slider_speed'],
+        'jbfjsliderpause' => $slider_settings['slider_pause'],
+        'jbfjslideruseCSS' => boolval($slider_settings['slider_useCSS']),
+        'jbfjsliderpager' => boolval($slider_settings['slider_pager']),
+        'jbfjslidercontrols' => boolval($slider_settings['slider_controls']),
+        'jbfjsliderhover' => boolval($slider_settings['slider_hover']),
+        'jbfjsliderticker' => boolval($slider_settings['slider_ticker']),
+        'jbfjslidertHover' => boolval($slider_settings['slider_tHover']),
+        'jbfjsliderminSlides' => $slider_settings['slider_minSlides'],
+		'jbfjslidermaxSlides' => $slider_settings['slider_maxSlides'],
+		'jbfjsliderslideWidth' => $slider_settings['slider_slideWidth']
+    );  
+    
+    // Load necessary scripts for use with slider
+    wp_enqueue_script('bxslider_script');
+    wp_enqueue_script('jbfj-slider_script');
+    wp_enqueue_style('jbfjslider_styles');
 }
 
-//Localize Settings for use in Slider JS
+// Localize Settings for use in Slider JS
 add_action( 'wp_footer', 'jbfj_slider_localize' );
 
 function jbfj_slider_localize() {
-	global $slideshows;	
-	
-	if ( !is_admin() ) {
-	
-		wp_localize_script('jbfj-slider_script', 'jbfjslidersettings', $slideshows);
-	}
+    global $slideshows;
+
+    if ( !is_admin() ) {
+        wp_localize_script('jbfj-slider_script', 'jbfjslidersettings', $slideshows);
+    }
 }
+
+// Custom Shortcode
+function jbfj_slider_shortcode($atts) {
+    extract(shortcode_atts(array(
+        'slug' => ''
+    ), $atts) );
+    $slug = "{$slug}";
+    ob_start(); // Output buffering not entirely necessary, but handy for waiting on large chunks of return html
+    $result = jbfj_slider($slug);
+    $result = ob_get_clean();
+    return $result;
+}
+
+/* rename to match above */
+add_shortcode("slider", "jbfj_slider_shortcode");
